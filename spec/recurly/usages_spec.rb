ed04aa9dc5b40bf75690d116b5f6d246812a87eb
Benjamin Eckel <bhelx@simst.im>
2016-04-20 12:33:32 -0500

    add usage serialization test

--- spec/recurly/usages_spec.rb
+++ spec/recurly/usages_spec.rb
@@ -10,17 +10,38 @@ describe Usage do
     )
   end
 
-  describe ".save" do
-    it "must be able to build and save a usage from a susbcription addon" do
-      subscription = Subscription.find 'abcdef1234567890'
+  let(:usage) do
+    subscription = Subscription.find 'abcdef1234567890'
 
-      # Select your add on by the code you want
-      sub_add_on = subscription.subscription_add_ons.detect do |add_on|
-        add_on.add_on_code == 'marketing_email'
-      end
+    # Select your add on by the code you want
+    sub_add_on = subscription.subscription_add_ons.detect do |add_on|
+      add_on.add_on_code == 'marketing_email'
+    end
 
-      usage = sub_add_on.usages.build
+    sub_add_on.usages.build
+  end
+
+  describe ".build" do
+    it "must be able to build" do
       usage.must_be_instance_of Usage
+    end
+  end
+
+  describe ".to_xml" do
+    it "must serialize to correct xml" do
+      time = DateTime.now
+      usage.amount = 10
+      usage.merchant_tag = "10 emails delivered for merchant"
+      usage.recording_timestamp = time
+      usage.usage_timestamp = time
+      usage.to_xml.must_equal <<XML.chomp
+<usage>\
+<amount>10</amount>\
+<merchant_tag>10 emails delivered for merchant</merchant_tag>\
+<recording_timestamp>#{time}</recording_timestamp>\
+<usage_timestamp>#{time}</usage_timestamp>\
+</usage>
+XML
     end
   end
 end
